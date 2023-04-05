f5a60139f0d0941b4c4dd5ee0f7b630beddd72cd
sarad chhetri <61595122+saradchhetri07@users.noreply.github.com>
2023-04-05 22:22:40 +0545

    Update Theme.dart

--- Theme.dart
+++ Theme.dart
@@ -1,4 +1,4 @@
-We would want to have unison of theme all accross our apps and for that we will have to use theme in our app
+1.We would want to have unison of theme all accross our apps and for that we will have to use theme in our app
 Example:
   theme: ThemeData(
           primarySwatch: Colors.green,
@@ -28,4 +28,21 @@ Example:
                           fontWeight: FontWeight.bold))
                   .titleLarge))
                   
+   2.Bottom Modal Sheet
+    
+    if we want to fire up modal sheet from bottom we can make use of Bottom Modal sheet
       
+      showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: userForm(_addTransaction),
+            // behavior: HitTestBehavior.opaque,
+          );
+        });
+3.To get out of the front screen 
+  
+     Navigator.of(context).pop();
+
+
